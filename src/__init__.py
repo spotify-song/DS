917eaa68adb10570e32cee6ec3ae0eb5d38aeec5
AVData <AVData@users.noreply.github.com>
2020-08-13 23:39:50 -0700

    established file stracture, connected to spotify API

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# Entry point for spoti app
+# To run app localy and generate access to data run 
+# the following line of code int he terminal:
+# FLASK_APP=src:APP flask run
+
+# Need to actually create an app.py file that generates the app 
+
+# from .app import create_app  # imports the app creation function
+
+# APP = create_app()  # should generate app to be ran
