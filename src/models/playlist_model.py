641f0238093efe1df032d6ecb9281d53268af504
AVData <AVData@users.noreply.github.com>
2020-08-23 23:49:22 -0700

    Fast

--- src/models/playlist_model.py
+++ src/models/playlist_model.py
@@ -14,19 +14,22 @@ router = APIRouter()
 class User(BaseModel):
     '''User ID'''
     user_id_1: str = Field(..., example='avargas-274')
-    user_id_2: str = Field(..., exmaple='spotify')
+    # user_id_2: str = Field(..., exmaple='spotify')
+
 
 class UserGroup(BaseModel):
     '''collection of user ids'''
     user_ids: List[User] = Field(..., example=['user id 1', 'user id 2'])
 
+
 class PlaylistURI(BaseModel):
     '''The URIs that will be generated from the analysis'''
     playlist_uri: str = Field(..., example='a long string of digits')
 
+
 # route to get user IDs
-@router.get('/users/{something else}')
-async def users(playlisturi: str):
+@router.get('/users/{user_id_1,user_id_2}')
+async def users(user_id_1: User, user_id_2: User):
     '''
     This function takes in a series of user IDs
     ### Path Parameter
@@ -35,11 +38,11 @@ async def users(playlisturi: str):
     ### Response
     'playlist_uri': string of alphanumeric values that generate a playlist
     '''
-    return playlisturi
+    return {'user_ids': [user_id_1, user_id_2]}
 
 
-@router.get('/viz/{user_ids}')
-async def viz(user_ids: str):
+@router.post('/uri/')
+async def URI(playlist_uri: PlaylistURI):
     '''
     This function will return the playlist uri generated from the given users
     ### Path Parameter
@@ -48,5 +51,5 @@ async def viz(user_ids: str):
     ### Reponse
     URI code to navigate user to the playlist generated.
     '''
-    return user_ids
+    return {'Playlist URI': playlist_uri.playlist_uri}
 # get token from both users to be able to access both of their libraries
