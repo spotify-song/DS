99a02c48abbfd3f7fb688170dfdae26b42a709b6
AVData <AVData@users.noreply.github.com>
2020-09-11 19:44:43 -0700

    generated DB test for table update class

--- api/data/my_db.py
+++ api/data/my_db.py
@@ -2,20 +2,33 @@ from os import getenv
 from dotenv import load_dotenv
 import psycopg2
 
-load_dotenv()
 
-# connecting to DB
-conn = psycopg2.connect(user=getenv('DB_USER'),
-                        password=getenv('DB_PASSWORD'),
-                        host=getenv('DB_HOST'),
-                        port=getenv('DB_PORT'),
-                        database=getenv('DB_DATABASE'))
+class UpdateTables():
+    def __init__(self):
+        load_dotenv()
+        # Connecting to DB
+        self.conn = psycopg2.connect(user=getenv('DB_USER'),
+                                     password=getenv('DB_PASSWORD'),
+                                     host=getenv('DB_HOST'),
+                                     port=getenv('DB_PORT'),
+                                     database=getenv('DB_DATABASE'))
 
-curs = conn.cursor()
-# print PostgresQL Connection properties
-print(conn.get_dsn_parameters(), "\n")
-curs.execute("SELECT version();")
-record = curs.fetchone()
+    def update_user(self, table_name='users', token, user_id, display_name):
+        '''
+        Method that updates the user table one user at a time.
 
-curs.close()
-conn.close()
+        Input:
+            - table_name: Table name is already in the arg, and does not need
+              to be changed
+            - token: Alphanumeric string of characters used to access user
+              information
+            - user_id: Alphanumeric string of characters used to identify users
+            - display_name: User profile display name
+
+        Output:
+            - There is no output necessarily, this function will be used to
+              update the user table with the pertinent information
+        '''
+        # psudo code: connect to db, check if user exists, if not the add user
+        conn = self.conn
+        table_name = table_name
