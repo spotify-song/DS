719a1d5517ab483b0f088dde73cad5010103f164
AVData <AVData@users.noreply.github.com>
2020-10-04 23:52:36 -0700

    setting up config envvars

--- __init__.py
+++ __init__.py
@@ -1,1 +1,17 @@
+# Entry point for spoti app
+# To run app localy and generate access to data run
+# the following line of code int he terminal:
+# running with FastAPI this time
 # uvicorn main:app --reload
+
+'''Entry point for FastAPI application.'''
+
+# imports the app creation function
+
+# Initialize FastAPI app
+
+# uvicorn main:app --reload
+
+from main import create_app
+
+APP = create_app()
--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 # uvicorn main:app --reload
-
+from 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from dotenv import load_dotenv
 
 from api.visualization import visualize
+# from visualization import visualize
 from api.models import playlist_model
+# from models import playlist_model
 
-app = FastAPI(
-             tltle='ML API',
-             description='API for Spotify playlist generator.  The playlists are generated from two users, and their most related interests.',
-             version='0.1',
-             docs_url='/',
-             )
+load_dotenv()
 
 
-app.include_router(playlist_model.router)
+def create_app():
 
-# may potentially be used to return data
-# app.include_router(.router)
+    app = FastAPI(
+                 tltle='ML API',
+                 description='API for Spotify playlist generator.\
+                             The playlists are generated from two users, and\
+                             their most related interests.',
+                 version='0.1',
+                 docs_url='/',
+                 )
 
+    app.include_router(playlist_model.router)
 
+    # may potentially be used to return data
+    # app.include_router(.router)
 
+    app.add_middleware(
+                      CORSMiddleware,
+                      allow_origins=['*'],
+                      allow_credentials=True,
+                      allow_methods=['*'],
+                      allow_headers=['*'],
+                      )
+    return app
 
-app.add_middleware(
-                  CORSMiddleware,
-                  allow_origins=['*'],
-                  allow_credentials=True,
-                  allow_methods=['*'],
-                  allow_headers=['*'],
-                  )
 
 if __name__ == '__main__':
     uvicorn.run(app)
