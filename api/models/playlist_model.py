01d219c4b2150ab2fe630309bc2311c1d4f82d71
AVData <AgustinVargas@ProtonMail.com>
2020-12-02 01:34:41 -0800

    add_or_update_user_data route works!

--- api/models/playlist_model.py
+++ api/models/playlist_model.py
@@ -28,36 +28,35 @@ router = APIRouter()
 user_person = UserData()
 
 
-class Users(BaseModel):
-    """User ID."""
-
-    user_id_1: str = Field(..., example='avargas-274')
-
-
-class UserToken(BaseModel):
-    """The URIs that will be generated from the analysis."""
-
-    user_token: dict = Field(..., example={
-                                        "access_token": "randome string",
-                                        "token_type": "Bearear or som",
-                                        "expires_in": "some time in sec",
-                                        "scope": "see spotify dev site",
-                                        "expires_at": "timeline",
-                                        "refreh_token": "random strings again"
-                                        })
+# class Users(BaseModel):
+#     """User ID."""
+#
+#     user_id_1: str = Field(..., example='avargas-274')
+#
+#
+# class UserToken(BaseModel):
+#     """The URIs that will be generated from the analysis."""
+#
+#     user_token: dict = Field(..., example={
+#                                         "access_token": "randome string",
+#                                         "token_type": "Bearear or som",
+#                                         "expires_in": "some time in sec",
+#                                         "scope": "see spotify dev site",
+#                                         "expires_at": "timeline",
+#                                        "refreh_token": "random strings again"
+#                                         })
 
 
 # route updates user data
 @router.post('/add_or_update_user_data/{refresh_token,user_id}')
-async def users(refresh_token, user_id):
+async def user_auth(refresh_token, user_id):
     """Update user track and token info.
 
     ### Path Parameter
-    'refresh_token': provide the API path paramter only the string of alphanumeric
-    values, and nothing more.
+    'refresh_token': string of alphanumeric values, and nothing more
 
-    'user_id': The API requires the user_id parameter in order to obtain
-    pertinant data for analysis.
+    'user_id': The API requires the user_id in order to obtain pertinant data
+    for analysis.
 
     ### Response
     'NA': still undefined, but will be determined shortly
@@ -84,7 +83,7 @@ async def users(refresh_token, user_id):
                                                 )
     track_table_update = user_person.track_db_update(
                                                     track_features,
-                                                    user_data['session']
+                                                    user_data['db_session']
                                                     )
 
     return {
@@ -96,14 +95,28 @@ async def users(refresh_token, user_id):
 # User overall stats
 @router.post('/overall_stats/{user_id}')
 async def overall_stats(user_id):
-    """Stats based on user's top listening history."""
+    """Stats based on user's top listening history.
+
+    ### Path Parameter
+    'user_id':
+
+    ### Response
+    'all_music_stats':
+    """
     return {f"Supposed to return {user_id}'s top stats of all time."}
 
 
 # generates user visuals and stats
 @router.post('/user_top_track_stats/{user_id}')
 async def stats(user_id):
-    """Generate user statistics and visualizations."""
+    """Generate user statistics and visualizations.
+
+    ### Path Parameter
+    'user_id':
+
+    ### Response
+    'top_track_stats':
+    """
     return {f"{user_id}'s stats go here"}
 
 
@@ -113,12 +126,20 @@ async def compatabilities(user_id_1, user_id_2):
 
     The compatabilities are generated based on the statistics from the user's
     libraries, genres and other music listening tendencies.
+
+    ### Path Parameter
+    'user_id_1':
+    'user_id_2':
+
+    ### Respnose
+    'compatability score':
     """
+    return {"The Compatability score for two distinct users"}
 
 
 # Generates user playlist
-@router.post('/uri/{playlistname}')
-async def auth(playlistname):
+@router.post('/new_playlist/{playlistname}')
+async def new_playlist(playlistname):
     """Return the playlist uri generated from the given users.
 
     ### Path Parameter
@@ -128,4 +149,3 @@ async def auth(playlistname):
     URI code to navigate user to the playlist generated.
     """
     return {"updates database with all of the user's music"}
-# get token from both users to be able to access both of their libraries
